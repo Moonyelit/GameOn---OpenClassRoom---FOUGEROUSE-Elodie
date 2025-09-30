a9c1accb8b1c4f3e885e690e90720db5e1b98327
TonPseudoExact <fougerouse.elodie@gmail.com>
2025-09-30 22:44:23 +0200

    🎨🚚 Déplacement page CSS + JS ajout fermeture page fonction +  animation de fermeture + modif visuel de la pop up

--- modal.js
+++ modal.js
@@ -11,13 +11,25 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
+// close modal event
+closeBtn.addEventListener("click", closeModal);
+
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.classList.add("show");
+}
+
+// close modal form
+function closeModal() {
+  modalbg.classList.add("closing");
+  setTimeout(() => {
+    modalbg.classList.remove("show", "closing");
+  }, 800); // 800ms correspond à la durée de l'animation définie dans --modal-duration
 }
 
 
